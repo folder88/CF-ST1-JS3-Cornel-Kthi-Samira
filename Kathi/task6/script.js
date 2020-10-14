936ac502d4ae6c43c535cb7bcc0b1c4ab9399173
Scheyk <kathrin.dostal@gmail.com>
2020-10-14 16:11:55 +0200

    change hex to rgb

--- Kathi/task6/script.js
+++ Kathi/task6/script.js
@@ -1,16 +1,22 @@
 // Create a JavaScript function to create random background colors each time we refresh the page.
 
-let hexCol = "0123456789ABCDEF";
+// let hexCol = "0123456789ABCDEF";
 let click = document.getElementById('click');
 
 click.addEventListener('click', colorChange);
+// console.log(hexCol);
 
 function colorChange() {
-    let color = "";
+    let colorOne = Math.floor(Math.random() * 256);
+    let colorThree = Math.floor(Math.random() * 256);
+    let colorFour = Math.floor(Math.random() * 256);
 
-    for (let i = 0; i < 6; i++) {
-        color += hexCol[Math.floor(Math.random() * 16)];
-    }
+    // for (let i = 0; i < 6; i++) {
+    //     color += hexCol[Math.floor(Math.random() * 16)];
 
-    document.body.style.backgroundColor = "#" + color;
+    //     console.log(color);
+    // }
+    // console.log(color);
+    // document.body.style.backgroundColor = "#" + color;
+    document.body.style.backgroundColor = `rgb(${colorOne},${colorThree},${colorFour})`;
 }
